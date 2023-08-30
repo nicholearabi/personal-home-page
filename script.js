8dd700e735dafe401f85edc15adb1fa187985ec8
Nichole Arabi <126015992+nicholearabi@users.noreply.github.com>
2023-08-30 18:18:15 +0100

    fixing bugs

--- script.js
+++ script.js
@@ -89,3 +89,31 @@ document.getElementById('button_remove').addEventListener('click', (event) => {
   });
 
 });
+
+
+var comments;
+
+function convertToNumber(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return 0
+  } else {
+    return number_value
+  }
+}
+
+
+comments = [];
+
+
+document.getElementById('enter-btn').addEventListener('click', (event) => {
+  event.preventDefault();
+  comments.push(document.getElementById('comment').value);
+  let element_list_comment = document.getElementById('list-comment');
+  let new_li = document.createElement('li');
+  new_li.innerText = comments.slice(-1)[0];
+
+  element_list_comment.appendChild(new_li);
+
+});
