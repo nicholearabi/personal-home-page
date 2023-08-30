021037a6d07594e2b10b95d6a6bb2f56cf14af73
Nichole Arabi <126015992+nicholearabi@users.noreply.github.com>
2023-08-30 17:42:05 +0100

    add roll the dice to codes

--- script.js
+++ script.js
@@ -1,33 +1,4 @@
-function getWords(type) {
-  // Return words of a given type, or all words if type is 'WORD'
-  let words = [
-    {type: 'ADJECTIVE', value: 'big'},
-    {type: 'ADJECTIVE', value: 'purple'},
-    {type: 'ADJECTIVE', value: 'new'},
-    {type: 'ADJECTIVE', value: 'interesting'},
-    {type: 'ADJECTIVE', value: 'curious'},
-    {type: 'ADJECTIVE', value: 'happy'},
-    {type: 'ADJECTIVE', value: 'busy'},
-    {type: 'ADJECTIVE', value: 'tiny'},
-    {type: 'NOUN', value: 'umbrella'},
-    {type: 'NOUN', value: 'knee'},
-    {type: 'NOUN', value: 'banana'},
-    {type: 'NOUN', value: 'platypus'},
-    {type: 'NOUN', value: 'cat'},
-    {type: 'NOUN', value: 'mouse'},
-    {type: 'NOUN', value: 'house'},
-    {type: 'VERB', value: 'impressed'},
-    {type: 'VERB', value: 'honoured'},
-    {type: 'VERB', value: 'saw'},
-    {type: 'VERB', value: 'ate'},
-    {type: 'VERB', value: 'surprised'},
-    {type: 'VERB', value: 'annoyed'},
-    {type: 'VERB', value: 'touched'},
-    {type: 'VERB', value: 'understood'},
-    {type: 'VERB', value: 'taught'},
-  ];
-  return words.filter(word => type === 'WORD' || word.type === type).map(word => word.value);
-}
+var total, dice, sum, item;
 
 function randomInt(n) {
   // Return a random number from in [0, n[
@@ -39,43 +10,82 @@ function randomMember(arr) {
   return arr[randomInt(arr.length)]
 }
 
-
-
-document.getElementById('noun1').addEventListener('click', (event) => {
-  event.target.innerText = randomMember(getWords('NOUN'));
-
-});
-
-document.getElementById('verb').addEventListener('click', (event) => {
-  event.target.innerText = randomMember(getWords('VERB'));
-
-});
-
-document.getElementById('adjective').addEventListener('click', (event) => {
-  event.target.innerText = randomMember(getWords('ADJECTIVE'));
-
-});
-
-document.getElementById('noun2').addEventListener('click', (event) => {
-  event.target.innerText = randomMember(getWords('NOUN'));
-
-});
-
-document.getElementById('daymode').addEventListener('click', (event) => {
-  let element_list = document.getElementById('list');
-  element_list.style.backgroundColor = '#ffffff';
-  element_list.style.color = '#000000';
-
-});
-
-document.getElementById('nightmode').addEventListener('click', (event) => {
+// Describe this function...
+function display_the_rolls() {
+  if(--window.LoopTrap <= 0) throw "Infinite loop.";
   let element_list2 = document.getElementById('list');
-  element_list2.style.backgroundColor = '#000000';
-  element_list2.style.color = '#ffffff';
+  sum.push(randomMember(dice));
+  let new_li = document.createElement('li');
+  new_li.innerText = sum.slice(-1)[0];
+
+  element_list2.appendChild(new_li);
+  let element_total2 = document.getElementById('total');
+  element_total2.innerText = sum.reduce((a,b) => a+b, 0);
+  total = sum.reduce((a,b) => a+b, 0);
+  if (total > 11) {
+    let element_info2 = document.getElementById('info');
+    element_info2.innerText = 'you lost';
+  } else if (total == 11) {
+    let element_info3 = document.getElementById('info');
+    element_info3.innerText = 'you won';
+  } else {
+    let element_info4 = document.getElementById('info');
+    element_info4.innerText = 'Keep playing!';
+  }
+}
+
+
+dice = [1, 2, 3, 4, 5, 6];
+sum = [];
+
+
+document.getElementById('button_restart').addEventListener('click', (event) => {
+  sum = [];
+  let element_list = document.getElementById('list');
+  element_list.replaceChildren();
+  let element_total = document.getElementById('total');
+  element_total.innerText = 0;
+  let element_info = document.getElementById('info');
+  element_info.innerText = 'Keep playing!';
 
 });
 
-document.getElementById('contact').addEventListener('click', (event) => {
-  event.target.innerText = '07099085028';
+document.getElementById('button_roll').addEventListener('click', (event) => {
+  display_the_rolls();
+
+});
+0 > 11;
+
+total;
+
+
+document.getElementById('button_remove').addEventListener('click', (event) => {
+  sum.pop();
+  let element_total3 = document.getElementById('total');
+  element_total3.innerText = sum.reduce((a,b) => a+b, 0);
+  total = sum.reduce((a,b) => a+b, 0);
+  if (total > 11) {
+    let element_info5 = document.getElementById('info');
+    element_info5.innerText = 'you lost';
+  } else if (total == 11) {
+    let element_info6 = document.getElementById('info');
+    element_info6.innerText = 'you won';
+  } else {
+    let element_info7 = document.getElementById('info');
+    element_info7.innerText = 'Keep playing!';
+  }
+
+});
+
+document.getElementById('button_remove').addEventListener('click', (event) => {
+  let element_list3 = document.getElementById('list');
+  element_list3.replaceChildren();
+  let element_list4 = document.getElementById('list');
+  sum.forEach((item) => {
+    let new_li2 = document.createElement('li');
+    new_li2.innerText = item;
+
+    element_list4.appendChild(new_li2);
+  });
 
 });
